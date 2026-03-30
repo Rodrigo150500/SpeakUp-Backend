f07442a01e39f9037d1c939b105f0186f683900b
Rodrigo150500 <rodrigo.takara1505@gmail.com>
2026-03-29 22:58:27 -0300

    Refact: refact register_user_validation

--- src/validation/register_user_validation.ts
+++ src/validation/register_user_validation.ts
@@ -1,29 +1,16 @@
 import * as z from "zod"
 import { IUser } from "../types/user";
 
-export function register_user_validation({name, email, password, section, grade, role}: IUser){
+export function register_user_validation(data: unknown) {
     
     const user_schema = z.object({
         name: z.string(),
-        email: z.email(),
+        email: z.string().email(),
         password: z.string(),
         section: z.string(),    
         grade: z.string(),
         role: z.string()
     })
 
-    try{        
-        
-        const user_validation = user_schema.parse({name, email, password, section, grade, role})
-
-        console.log(user_validation)
-
-    }catch(error){ 
-        
-        if(error instanceof z.ZodError){
-            console.log(error.issues)
-        }
-
-    }
+    return user_schema.parse(data) // 🔥 RETORNA
 }
-
