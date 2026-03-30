dd81e14a9a07238c38de84baae56e98107bb31c8
Rodrigo Takara <Rodrigo.takara1505@gmail.com>
2026-03-30 17:11:12 -0300

    Change: delete string() from email

--- src/validation/register_user_validation.ts
+++ src/validation/register_user_validation.ts
@@ -4,12 +4,12 @@ export function register_user_validation(data: unknown) {
     
     const user_schema = z.object({
         name: z.string(),
-        email: z.string().email(),
+        email: z.email(),
         password: z.string(),
         section: z.string(),    
         grade: z.string(),
         role: z.string()
     })
 
-    return user_schema.parse(data) // 🔥 RETORNA
+    return user_schema.parse(data) 
 }
