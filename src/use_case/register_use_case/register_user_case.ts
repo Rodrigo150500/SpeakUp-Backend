e936433232d65cc262cf6311237226cec19d4b8f
Rodrigo Takara <Rodrigo.takara1505@gmail.com>
2026-04-03 11:02:15 -0300

    In progress

--- src/use_case/register_use_case/register_user_case.ts
+++ src/use_case/register_use_case/register_user_case.ts
@@ -1,8 +1,9 @@
 import { IUserRepository } from "../../model/postgre/interfaces/user_repository_interface";
+import { StudentUser, TeacherUser } from "../../model/postgre/types/user_repository_output";
 
 import { IRegisterUseCase } from './interface/register_interface';
 
-import { RegisterUserDTO, RegisterUserResponseDTO, StudentDTO, TeacherDTO } from './register_user_dto';
+import { RegisterUserDTO, RegisterUserResponseDTO, TeacherResponseDTO, StudentResponseDTO, StudentDTO, TeacherDTO } from './register_user_dto';
 
 export class RegisterUserUseCase implements IRegisterUseCase{
 
@@ -18,10 +19,17 @@ export class RegisterUserUseCase implements IRegisterUseCase{
 
             const response = await this.insert_student_in_database(data)
 
+            const formatted_response = this.format_response_student(response)
+
+            return formatted_response
+
         }else{
 
             const response = await this.insert_teacher_in_database(data)
 
+            const formatted_resposne = this.format_response_teacher(response)
+
+            return formatted_resposne
         }        
 
 
@@ -36,6 +44,28 @@ export class RegisterUserUseCase implements IRegisterUseCase{
     private async insert_teacher_in_database(data: TeacherDTO){
 
         return await this.repository.createTeacher(data)
+    }
+
+    private format_response_student(data: StudentUser){
+        return {
+            data:{
+                operation: "Insert",
+                count: 1,
+                attributes: {
+                    
+                }
+            },status_code: 201
+        }
+    }
+
+    private format_response_teacher(data: TeacherUser){
+       return {
+            data:{
+                operation: "Insert",
+                count: 1,
+                attributes: data
+            },status_code: 201
+        }
     }
 
     
--- src/use_case/register_use_case/register_user_dto.ts
+++ src/use_case/register_use_case/register_user_dto.ts
@@ -1,4 +1,5 @@
-import {Prisma} from "../../../generated/prisma/client"
+import { Role } from "../../../generated/prisma/enums"
+import { StudentUser } from "../../model/postgre/types/user_repository_output"
 
 type BaseRegisterDTO = {
     name: string
@@ -21,19 +22,45 @@ export type TeacherDTO = BaseRegisterDTO & {
 
 export type RegisterUserDTO = TeacherDTO | StudentDTO
 
-export type StudentUser = Prisma.UserGetPayload<{
-  include: { student: true }
-}> & { teacher: null }
+//------------------------------------------------
 
-export type TeacherUser = Prisma.UserGetPayload<{
-  include: { teacher: true }
-}> & { student: null }
+export type StudentResponseDTO = {
+
+  id: string
+  name: string
+  email: string
+  password: string
+  created_at: Date
+  role: Role,
+  student: {
+    id: string
+    user_id: string
+    grade: string 
+    section: string
+    number: number
+  }
+
+}
+
+export type TeacherResponseDTO = {
+  id: string
+  name: string
+  email: string
+  password: string
+  created_at: Date
+  role: Role,
+  teacher:{
+    id: string,
+    user_id: string
+  }
+}
+
 
 export type RegisterUserResponseDTO = {
-  data:{
-    operation: string,
-    count: number,
-    attributes: StudentUser | TeacherUser
+  data: {
+    operation: string
+    count: number
+    attributes: StudentResponseDTO | TeacherResponseDTO
   },
   status_code: number
 }
