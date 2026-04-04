64357214a50056d085c994c0a707bbf4eb4d9b52
Rodrigo Takara <Rodrigo.takara1505@gmail.com>
2026-04-04 15:56:03 -0300

    Add: Adding controler register needs test

--- src/controller/register_controller/register_controller.ts
+++ src/controller/register_controller/register_controller.ts
@@ -25,3 +25,4 @@ export class RegisterController implements IRegisterController{
   }
 
 }
+
--- src/validation/register_user_validation.ts
+++ src/validation/register_user_validation.ts
@@ -2,14 +2,22 @@ import * as z from "zod"
 
 export function register_user_validation(data: unknown) {
     
-    const user_schema = z.object({
+    const registerSchema = z.discriminatedUnion("role", [
+    z.object({
+        role: z.literal("STUDENT"),
         name: z.string(),
-        email: z.email(),
+        email: z.string(),
         password: z.string(),
-        section: z.string(),    
         grade: z.string(),
-        role: z.string()
+        section: z.string()
+    }),
+    z.object({
+        role: z.literal("TEACHER"),
+        name: z.string(),
+        email: z.string(),
+        password: z.string()
     })
+    ])
 
-    return user_schema.parse(data) 
+    return registerSchema.parse(data) 
 }
