865e4fc45f67f6b6c51d3475fe0f7631ebeebd85
Rodrigo Takara <Rodrigo.takara1505@gmail.com>
2026-04-04 09:21:49 -0300

    Add: adding teacher test for RegisterUserUseCase

--- src/use_case/register_use_case/register_user_case.test.ts
+++ src/use_case/register_use_case/register_user_case.test.ts
@@ -3,10 +3,10 @@ import { RegisterUserUseCase } from "./register_user_case";
 import { UserRepository } from "../../model/postgre/user_repository";
 
 import {expect, test} from "vitest"
-import { RegisterUserDTO, RegisterUserResponseDTO, StudentResponseDTO } from './register_user_dto';
-import { number } from 'zod';
 
-test("Should create student in database", async () => {
+import { RegisterUserDTO, TeacherDTO } from './register_user_dto';
+
+test.skip("Should create student in database", async () => {
   
   const repository = new UserRepository()
   const useCase = new RegisterUserUseCase(repository)
@@ -46,4 +46,42 @@ test("Should create student in database", async () => {
   if(response.data.attributes.role == 'STUDENT'){
     expect(response).toEqual(expected_response)
   }
+})
+
+test("Should create teacher in database", async ()=>{
+
+  const repository = new UserRepository()
+  const use_case = new RegisterUserUseCase(repository)
+
+  const teacher_data: TeacherDTO = {
+    name: "Rosalina",
+    email: "Rosalina.rs@gmail.com",
+    password: "123456",
+    role: "TEACHER"
+  }
+
+  const response = await use_case.handle(teacher_data)
+
+  const expected_response = {
+    data:{
+      operation: "Insert",
+      count:1,
+      attributes:{
+        id: expect.any(String),
+        name: teacher_data.name,
+        email: teacher_data.email,
+        created_at: expect.any(Date),
+        role: "TEACHER",
+        teacher:{
+          id: expect.any(String),
+          user_id: expect.any(String)
+        }
+      }
+    },
+    status_code: 201
+  }
+
+  expect(response).toEqual(expected_response)
+
+  
 })
